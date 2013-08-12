d71a7e85773be881c200f406ac534a7c7cf943c0
Rafael Gonzalez Cabero <rafael.gonzalez.cabero@gmail.com>
2013-08-12 17:28:06 +0200

    Added the epni demo users setup for the cassandra DAOs

--- src/main/java/epnoi/model/dao/cassandra/SimpleTestSetUp.java
+++ src/main/java/epnoi/model/dao/cassandra/SimpleTestSetUp.java
@@ -16,13 +16,13 @@ public class SimpleTestSetUp {
 		userCassandraDAO.init();
 
 		System.out.println(" --------------------------------------------");
-		System.out.println("-- "+searchCassandraDAO.getSearchs());
-		for (User user:userCassandraDAO.getUsers()){
-			System.out.println(user.getName()+ " existe!");
+		System.out.println("-- " + searchCassandraDAO.getSearchs());
+		for (User user : userCassandraDAO.getUsers()) {
+			System.out.println(user.getName() + " existe!");
 			userCassandraDAO.delete(user.getURI());
 		}
-		
-		//Let's create the users
+
+		// Let's create the users
 		User user = new User();
 		user.setURI("http://userRafa");
 		user.setName("Rafa");
@@ -38,33 +38,54 @@ public class SimpleTestSetUp {
 		userElOtro.addSearch("http://searchC");
 		userElOtro.addSearch("http://searchD");
 		userCassandraDAO.create(userElOtro);
-		
+
+		User pique = new User();
+		pique.setURI("http://userPique");
+		pique.setName("Pique");
+		pique.setPassword("PasswordDePique");
+		pique.addSearch("http://searchE");
+		pique.addSearch("http://searchF");
+		userCassandraDAO.create(pique);
+
 		User readUser = userCassandraDAO.read("http://userRafa");
 		System.out.println("readed user> " + readUser);
 
 		User otherReadUser = userCassandraDAO.read("http://userSara");
 		System.out.println("readed user> " + otherReadUser);
 		System.out.println("Exiting test");
-		//Let's create the searchs
-		
-		for (Search search:searchCassandraDAO.getSearchs()){
-			System.out.println(search.getTitle()+ "existe!");
+		// Let's create the searchs
+
+		for (Search search : searchCassandraDAO.getSearchs()) {
+			System.out.println(search.getTitle() + "existe!");
 			searchCassandraDAO.delete(search.getURI());
 		}
-		
-		
-		
-		for (String label : Arrays.asList("A","B","C","D")){
-			String searchURI= "http://search"+label;
+
+		for (String label : Arrays.asList("A", "B", "C", "D", "E", "F")) {
+			String searchURI = "http://search" + label;
 			Search search = new Search();
 			search.setURI(searchURI);
-			search.setDescription("Search"+label +" Description");
-			search.setTitle("Search"+label);
-			for (String expressionLabel : Arrays.asList("A","B","C","D")){
-				search.addExpression("expression"+expressionLabel);
+			search.setDescription("Search" + label + " Description");
+			search.setTitle("Search" + label);
+			/*
+			 * for (String expressionLabel : Arrays.asList("A", "B", "C",
+			 * "D","E","F")) { search.addExpression("expression" +
+			 * expressionLabel); }
+			 */
+
+			if (search.getURI().equals("http://searchE")) {
+				search.addExpression("galaxy");
+			} else {
+
+				for (String expressionLabel : Arrays.asList("A", "B", "C", "D",
+						"E", "F")) {
+					search.addExpression("expression" + expressionLabel);
+				}
+
 			}
 			searchCassandraDAO.create(search);
-			
 		}
+
+		Search piqueSearch = searchCassandraDAO.read("http://searchE");
+		piqueSearch.addExpression("galaxy");
 	}
 }
